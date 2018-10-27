2e5bb88d2cfcb08bacf7bbe2593eff11ba75c587
bernatferragut <elbernat@gmail.com>
2018-10-27 17:05:00 -0400

    midi fighter update

--- midiFighter/js/main.js
+++ midiFighter/js/main.js
@@ -14,6 +14,7 @@
 // }
 
 
+// All at the same time
 // Array.prototype.slice.call(document.getElementsByClassName('preset4')).forEach(function(el) {
 //     new Knob(el, new Ui.P4());
 //     el.addEventListener('change', function  () {
@@ -23,10 +24,61 @@
 
 
 // Good programming
-let k1 = document.getElementById('k1');
-let knob = new Knob(k1, new Ui.P4());
-k1.addEventListener('change', function(){
-    console.log(k1.value);
-});
+// let k1 = document.getElementById('k1');
+// let knob = new Knob(k1, new Ui.P4());
+// k1.addEventListener('change', function(){
+//     console.log(k1.value);
+// });
 
+let rotation = 0;
+
+Array.prototype.slice.call(document.getElementsByClassName('preset4')).forEach(function(el) {
+    new Knob(el, new Ui.P4());
+    el.addEventListener('change', function  () {
+        rotation = el.value;
+        console.log(rotation);
+    })
+})
+
+// P5JS
+let canvas, w =380,  h = 150;
+let dot, s = 4;
+
+function setup(){
+    // Canvas creation
+    canvas = createCanvas(w, h);
+    // Move the canvas inside div with id'superCanvas'
+    canvas.parent('superCanvas');
+    background('rgb(10,10,10)');
+    dot = new Dot(w/2, 50, s);
+}
+
+function draw(){
+    background('rgb(10,10,10)');
+    dot.on();
+    dot.move();
+}
+
+// Dot object
+class Dot {
+    // class attributes
+    constructor(x, y, s) {
+      this.x = x | 0;
+      this.y = y | 0;
+      this.size = s | 2;
+      this.velX = 2;
+    }
+    // class methods
+    on() {
+      noStroke();
+      fill(color('white'));
+      ellipse(this.x, this.y, this.size, this.size);
+    }
+    move() {
+        this.x += this.velX;
+        if (this.x + s / 2 > w || this.x - s / 2 < 1) {
+          this.velX = this.velX * -1;
+        }
+    }
+  }
 
